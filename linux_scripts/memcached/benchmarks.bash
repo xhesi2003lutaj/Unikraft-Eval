a288a26f4e9b922ec650fd0ae704bedf02ddd416
xhesi2003lutaj <xh.lutaj@gmail.com>
2025-05-03 11:29:33 +0200

    benchmark

--- linux_scripts/memcached/benchmarks.bash
+++ linux_scripts/memcached/benchmarks.bash
@@ -1,3 +1,2 @@
+memtier_benchmark -s 127.0.0.1 -p 11211   --protocol=memcache_text   --threads=4 --clients=50   --test-time=30   --json-out-file=ubuntu_results.json
 
-# Measures how fast memcached responds to individual get and set operations
-memtier_benchmark -p 11211 -t 4 -c 50 -n 10000 --protocol=memcache_text
