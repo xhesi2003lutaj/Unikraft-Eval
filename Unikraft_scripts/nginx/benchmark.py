e65e36a76cefde59a2395fcc379492b686c4dd0b
xhesi2003lutaj <xh.lutaj@gmail.com>
2025-05-04 09:39:28 +0200

    nginx_ubuntu_unikraft

---
+++ Unikraft_scripts/nginx/benchmark.py
@@ -0,0 +1,11 @@
+import subprocess
+
+for i in range(5):
+    out_file = f"nginx_unikraft_run_100_{i}.txt"
+    with open(out_file, "w") as f:
+        subprocess.run(
+            ["wrk", "-t4", "-c100", "-d30s", "http://localhost:8080/"],
+            stdout=f
+        )
+
+
---
+++ linux_scripts/nginx/qemu.bash
@@ -0,0 +1,13 @@
+qemu-system-x86_64 \
+  -m 128M \
+  -smp cpus=1,threads=1,sockets=1 \
+  -cpu host,+x2apic,-pmu \
+  -netdev user,id=net0,hostfwd=tcp::8080-:80 \
+  -device virtio-net-pci,netdev=net0 \
+  -drive file=ubuntu.qcow2,format=qcow2 \
+  -cdrom seed.iso \
+  -enable-kvm \
+  -nographic \
+  -serial mon:stdio
+
+
